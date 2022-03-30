dc3f7b3d0dd4f377aacb7f528682633127119f23
mi2ne <show3070@gmail.com>
2022-03-31 07:25:02 +0900

    About 페이지

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'Mil2ne',
+    email: 'show3070@gmail.com',
+    blog: 'https://test.com',
+    phone: '+82-10-1234-5678',
+    image: 'https://avatars.githubusercontent.com/u/39588912?v=4'
+  })
+}
