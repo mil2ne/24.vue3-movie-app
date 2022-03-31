221d826a2011cb6238794ee3af63fef090520a53
mi2ne <show3070@gmail.com>
2022-03-31 20:09:51 +0900

    bootstarp 반응형 breakpoint

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -78,6 +78,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~/scss/main";
 
 .container {
   display: flex;
@@ -103,6 +104,23 @@ export default {
     height: 50px;
     font-weight: 700;
     flex-shrink: 0;
+  }
+  @include media-breakpoint-down(lg) {
+    display: block;
+    input {
+      margin-right: 0;
+      margin-bottom: 10px;
+    }
+    .selects {
+      margin-right: 0;
+      margin-bottom: 10px;
+      select {
+        width: 100%;
+      }
+    }
+    .btn {
+      width: 100%;
+    }
   }
 }
 </style>
--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -225,5 +225,33 @@ export default {
 
     }
   }
+  @include media-breakpoint-down(xl) {
+    .poster {
+      width: 300px;
+      height: 300px * 3 / 2;
+      margin-right: 40px;
+    }
+  }
+  @include media-breakpoint-down(lg) {
+    display: block;
+    .poster {
+      margin-bottom: 40px;
+    }
+  }
+  @include media-breakpoint-down(md) {
+    .specs {
+      .title {
+        font-size: 50px;
+      }
+      .ratings {
+        .rating-wrap {
+          display: block;
+          .rating {
+            margin-top: 10px;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
