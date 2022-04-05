1d20ad4a8cde6057d7666b7c0df27befd36fe537
mi2ne <show3070@gmail.com>
2022-04-06 07:16:33 +0900

    mock 테스트 1

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,9 +1,11 @@
-import { asyncFn } from "./example";
+import * as example from "./example";
 
-describe('비동기 테스트', () => {
+describe('비동기 모의함수 테스트', () => {
   
   test('async/await', async () => {
-    const res = await asyncFn()
+    jest.spyOn(example, 'asyncFn')
+      .mockResolvedValue('done!')
+    const res = await example.asyncFn()
     expect(res).toBe('done!')
   }, 7000)
 })
